d1c58ff0807db9d77965a751018c5e4aa2bc236b
Frank van den Thillart <thifm@hr.nl>
2026-04-22 19:45:14 +0200

    Ran eslint and refactored all code to pass it. Also generated tests to improve test coverage. All tests pass

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+// ESLint flat config for TypeScript projects (ESLint v9+)
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+        ecmaVersion: "latest"
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/explicit-module-boundary-types": "off"
+    },
+  },
+];
